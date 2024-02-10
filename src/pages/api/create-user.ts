57b1af4d4e8c6a31920231df2ffc4656b89db99d
Adebola Adeniran <onedebos@gmail.com>
2024-02-10 17:53:48 +0100

    create user route

--- src/pages/api/create-user.ts
+++ src/pages/api/create-user.ts
@@ -1,12 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
- 
+import axios from 'axios'
+
 type ResponseData = {
   message: string
 }
  
-export default function handler(
+export default async function createUser(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+  if (!req.body.userId) {
+    res.status(400).send("No userId provided");
+  }
+
+  const options = {
+    method: "POST",
+    url: "https://embedded.runalloy.com/2023-12/one/users",
+    headers: {
+      accept: "application/json",
+      Authorization: `bearer ${config.apiKey}`,
+      "content-type": "application/json",
+    },
+    data: { username: req.body.userId },
+  };
+  let response;
+  try {
+    response = await axios.request(options);
+    res.status(200).json({ message: response.data })
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Could not create user!' })
+  }
+
+  
 }
