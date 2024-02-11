cea3d5ac765b7f5541703bf542230134a53ee9b3
Adebola Adeniran <onedebos@gmail.com>
2024-02-11 14:14:42 +0100

    remove phone number from create-customer

--- src/app/create-customer/page.tsx
+++ src/app/create-customer/page.tsx
@@ -70,19 +70,6 @@ const CustomersList = () => {
               required
             />
           </div>
-          <div className="mb-5">
-            <label
-              htmlFor="phoneNumber"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Phone No:
-            </label>
-            <input
-              type="text"
-              id="phoneNumber"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            />
-          </div>
 
           <button
             type="submit"
